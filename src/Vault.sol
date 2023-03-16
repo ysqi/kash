fd2c5623ec2f4830b46dd9f5e91fc48c083a549e
zhuochujie <2662648243@qq.com>
2023-03-16 14:08:34 +0800

    基本环境设置

---
+++ src/Vault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault {
+
+    constructor() {
+        
+    }
+}
